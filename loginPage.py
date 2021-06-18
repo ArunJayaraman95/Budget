eac2df137e76584f981d4a21a5e4bb5da840912a
Arun Jayaraman <riparun.aj@gmail.com>
2021-06-18 17:03:23 -0400

    Added password input to login screen

--- loginPage.py
+++ loginPage.py
@@ -8,16 +8,22 @@ import csv
 root=Tk()
   
 # Set screen width (sx) and height (sy)
-sx= root.winfo_screenwidth() 
-sy= root.winfo_screenheight()
+sx = root.winfo_screenwidth() 
+sy = root.winfo_screenheight()
 
 # Set the window size
 root.geometry("%dx%d" % (sx, sy))
 root.title("Budget Boi")
 root.iconbitmap("img/WayneStateLogo.ico")
 
+# Define colors
 c1 = "#56C3A2"
 c2 = "#41927A"
+
+# Define fonts
+buttonFont = font.Font(size = 24)
+inputFont = font.Font(size = 20)
+
 # Create canvas
 canvas = Canvas(root, width = sx, height = sy)
 canvas.pack()
@@ -26,26 +32,27 @@ canvas.config(background=c1)
 # Form outline
 canvas.create_rectangle(sx/2 - 200, sy/6, sx/2 + 200, 2*sy/3-50, fill = c2, width = 0)
 
-
+# Create labels for login and place them
 usernameFont = font.Font(size = 16)
 usernameLabel = Label(root, text = "Username: ", font = usernameFont, bg = c2)
 usernameLabel.place(x = sx/2 - 97, y = sy/2 - 100, anchor = N)
 loginLabel = Label(root, text = "Login", font = usernameFont, bg = c2)
 loginLabel.place(x = sx/2, y = sy/2 - 350, anchor = N)
 loginLabel.config(font = ("Courier", 44))
-
+passwordLabel = Label(root, text = "Password: ", font = usernameFont, bg = c2)
+passwordLabel.place(x = sx/2 - 97, y = sy/2 - 20, anchor = N)
 
 # Logo image
 logo = ImageTk.PhotoImage(Image.open("img/Dollar Sign.png").resize((150, 150)))
 logoImage = Label(image = logo)
 logoImage.place(x = sx/2, y = sy/2 - 270, anchor = N)
 
-buttonFont = font.Font(size = 24)
-inputFont = font.Font(size = 20)
 
-# Create input box for username
+# Create input box for username and password
 uInput = Entry(root, width = 20, font = inputFont)
 uInput.place(x = sx/2, y = sy/2 - 60, anchor = N)
+pInput = Entry(root, width = 20, font = inputFont)
+pInput.place(x=sx/2, y = sy/2 + 20, anchor = N)
 
 
 activeUser = ""
@@ -89,7 +96,7 @@ def myClick():
 
 
 #Create button
-submitButton = Button(root, text = "Submit", bg = "#A9e451", padx = 20, pady = 10, font = buttonFont, command = myClick)
-submitButton.place(x = sx/2, y = sy/2, anchor = N)
+submitButton = Button(root, text = "Submit", bg = "#A9e451", padx = 10, pady = 0, font = ("Verdana", 15), command = myClick)
+submitButton.place(x = sx/2, y = sy/2 + 70, anchor = N)
 
 root.mainloop()
