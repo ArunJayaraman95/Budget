5d5c0be901a68707fd8234a3538780cccf3b53e3
Arun Jayaraman <riparun.aj@gmail.com>
2021-06-19 02:00:20 -0400

    Added registration frame to file
    Also made a 'test' account for testing purposes with the password 'password'

--- loginPage.py
+++ loginPage.py
@@ -4,17 +4,14 @@ import tkinter.font as font
 from PIL import ImageTk, Image
 import csv
 
+
+
+def showFrame(frame):
+    frame.tkraise()
+
 # Creating window
 root=Tk()
-  
-# Set screen width (sx) and height (sy)
-sx = root.winfo_screenwidth() 
-sy = root.winfo_screenheight()
-
-# Set the window size
-root.geometry("%dx%d" % (sx, sy))
-root.title("Budget Boi")
-root.iconbitmap("img/WayneStateLogo.ico")
+root.state("zoomed")  #Makes it fullscreen automatically
 
 # Define colors
 mainColor = "#56C3A2"
@@ -24,8 +21,33 @@ accentColor = "#57729E"
 buttonFont = font.Font(size = 24)
 inputFont = font.Font(size = 20)
 
+# Configurations
+root.rowconfigure(0, weight = 1)
+root.columnconfigure(0, weight = 1)
+
+loginFrame = Frame(root, background = mainColor)
+newAccountFrame = Frame(root, background = mainColor)
+
+for frame in (loginFrame, newAccountFrame):
+    frame.grid(row = 0, column = 0, sticky = "nsew")
+
+showFrame(loginFrame)
+
+# Set screen width (sx) and height (sy)
+sx = root.winfo_screenwidth() 
+sy = root.winfo_screenheight()
+
+# Set the window size
+root.geometry("%dx%d" % (sx, sy))
+root.title("Budget Boi")
+root.iconbitmap("img/WayneStateLogo.ico")
+
+#============Frame 1 ==============# 
+
+
+
 # Create canvas
-canvas = Canvas(root, width = sx, height = sy)
+canvas = Canvas(loginFrame, width = sx, height = sy)
 canvas.pack()
 canvas.config(background=mainColor)
 
@@ -34,25 +56,27 @@ canvas.create_rectangle(sx/2 - 200, sy/6, sx/2 + 200, 2*sy/3-50, fill = accentCo
 
 # Create labels for login and place them
 usernameFont = font.Font(size = 16)
-usernameLabel = Label(root, text = "Username: ", font = usernameFont, bg = accentColor)
+usernameLabel = Label(loginFrame, text = "Username: ", font = usernameFont, bg = accentColor)
 usernameLabel.place(x = sx/2 - 97, y = sy/2 - 110, anchor = N)
-loginLabel = Label(root, text = "Login", font = usernameFont, bg = accentColor)
+loginLabel = Label(loginFrame, text = "Login", font = usernameFont, bg = accentColor)
 loginLabel.place(x = sx/2, y = sy/2 - 350, anchor = N)
 loginLabel.config(font = ("Courier", 44))
-passwordLabel = Label(root, text = "Password: ", font = usernameFont, bg = accentColor)
+passwordLabel = Label(loginFrame, text = "Password: ", font = usernameFont, bg = accentColor)
 passwordLabel.place(x = sx/2 - 97, y = sy/2 - 40, anchor = N)
 
+
 # Logo image
-logo = ImageTk.PhotoImage(Image.open("img/Dollar Sign.png").resize((120, 120)))
-logoImage = Label(image = logo)
-logoImage.place(x = sx/2, y = sy/2 - 270, anchor = N)
+#logo = ImageTk.PhotoImage(Image.open("img/Dollar Sign.png").resize((120, 120)))
+#logoImage = Label(image = logo)
+#logoImage.place(x = sx/2, y = sy/2 - 270, anchor = N)
 
 
 # Create input box for username and password
-uInput = Entry(root, width = 20, font = inputFont)
+uInput = Entry(loginFrame, width = 20, font = inputFont)
 uInput.place(x = sx/2, y = sy/2 - 80, anchor = N)
-pInput = Entry(root, width = 20, font = inputFont, show = '*')
+pInput = Entry(loginFrame, width = 20, font = inputFont, show = '*')
 pInput.place(x=sx/2, y = sy/2 - 10, anchor = N)
+
 
 
 activeUser = ""
@@ -68,8 +92,7 @@ def popupLogin(message):
     b.grid(row=1, column=0)
 
 
-
-def myClick():
+def submitLogin():
     global activeUser
     print("Login submit button clicked")
     userEntry = uInput.get()
@@ -84,21 +107,98 @@ def myClick():
                 print("In file")
                 foundFlag = True
                 activeUser = userEntry
-                #popupLogin("Welcome " + line[0] + "!")
+                popupLogin("Welcome " + line[0] + "!")
                 break
         if not foundFlag:
             print("Username (" + userEntry + ") not found")
             #popupLogin("Username not found")
-            warningLabel = Label(root, text = "Incorrect credentials", fg = "#FF890A", bg = accentColor)
+            warningLabel = Label(loginFrame, text = "Incorrect credentials", fg = "#FF890A", bg = accentColor)
             warningLabel.config(font = ("Verdana", 12))
             warningLabel.place(x = sx/2 - 70, y = sy/2 + 30, anchor = N)
     uInput.delete(0, END)
     pInput.delete(0, END)
     print("Active user:", activeUser)
 
+#Create buttons
+submitButton = Button(loginFrame, text = "Submit", bg = "#A9e451", padx = 10, pady = 0, font = ("Verdana", 15), command = submitLogin)
+submitButton.place(x = sx/2 + 100, y = sy/2 + 70, anchor = N)
+registerButton = Button(loginFrame, text = "Make new account", font = ("Verdana", 10), bg = mainColor, command = lambda: showFrame(newAccountFrame))
+registerButton.place(x = sx/2 - 85, y = sy/2 + 80, anchor = N)
 
-#Create button
-submitButton = Button(root, text = "Submit", bg = "#A9e451", padx = 10, pady = 0, font = ("Verdana", 15), command = myClick)
-submitButton.place(x = sx/2, y = sy/2 + 70, anchor = N)
 
-root.mainloop()
+#===================Frame 2=====================#
+# Create canvas
+canvas = Canvas(newAccountFrame, width = sx, height = sy)
+canvas.pack()
+canvas.config(background=mainColor)
+
+# Form outline
+canvas.create_rectangle(sx/2 - 200, sy/6, sx/2 + 200, 2*sy/3-50, fill = accentColor, width = 0)
+
+
+# Create labels for login and place them
+loginLabel = Label(newAccountFrame, text = "Register!", font = usernameFont, bg = accentColor)
+loginLabel.place(x = sx/2, y = sy/2 - 350, anchor = N)
+usernameFont = font.Font(size = 16)
+usernameLabel = Label(newAccountFrame, text = "Username: ", font = usernameFont, bg = accentColor)
+usernameLabel.place(x = sx/2 - 155, y = sy/2 - 280, anchor = NW)
+loginLabel.config(font = ("Courier", 44))
+passwordLabel = Label(newAccountFrame, text = "Password: ", font = usernameFont, bg = accentColor)
+passwordLabel.place(x = sx/2 - 155, y = sy/2 - 210, anchor = NW)
+passwordConLabel = Label(newAccountFrame, text = "Confirm Password: ", font = usernameFont, bg = accentColor)
+passwordConLabel.place(x = sx/2 - 155, y = sy/2 - 140, anchor = NW)
+
+# Logo image
+#logo = ImageTk.PhotoImage(Image.open("img/Dollar Sign.png").resize((120, 120)))
+#logoImage = Label(image = logo)
+#logoImage.place(x = sx/2, y = sy/2 - 270, anchor = N)
+
+
+# Create input box for username and password
+uInput = Entry(newAccountFrame, width = 20, font = inputFont)
+uInput.place(x = sx/2, y = sy/2 - 250, anchor = N)
+pInput = Entry(newAccountFrame, width = 20, font = inputFont, show = '*')
+pInput.place(x=sx/2, y = sy/2 - 180, anchor = N)
+pcInput = Entry(newAccountFrame, width = 20, font = inputFont, show = '*')
+pcInput.place(x=sx/2, y = sy/2 - 110, anchor = N)
+
+
+def registerAccount():
+    print("Register account clicked")
+    userEntry = uInput.get()
+    passEntry = pInput.get()
+    confEntry = pcInput.get()
+    foundFlag = False
+    # Check username
+    with open('UserData/userList.csv', 'r') as file:
+        reader = csv.reader(file)
+        for line in reader:
+            print(line)
+            if line[0].lower().strip() == userEntry.lower():
+                print("User already exists")
+                popupLogin("USER Exists already")
+                foundFlag = True
+    if not foundFlag:
+        if passEntry != confEntry:
+            popupLogin("Passwords don't match")
+        else:
+            with open ('UserData/userList.csv', 'a') as file:
+                writer = csv.writer(file, lineterminator="\n")
+                writer.writerow([userEntry, passEntry])
+            print("User ", userEntry, "added!")
+            popupLogin("USER ADDED")
+            
+
+    uInput.delete(0, END)
+    pInput.delete(0, END)
+    pcInput.delete(0, END)
+
+#Create buttons
+confirmButton = Button(newAccountFrame, text = "Register", bg = "#A9e451", padx = 10, pady = 0, font = ("Verdana", 15), command = registerAccount)
+confirmButton.place(x = sx/2, y = sy/2 + 70, anchor = NW)
+returnButton = Button(newAccountFrame, text = "Return to login", font = ("Verdana", 10), bg = mainColor, command = lambda: showFrame(loginFrame))
+returnButton.place(x = sx/2 - 155, y = sy/2 + 80, anchor = NW)
+
+
+
+loginFrame.mainloop()
