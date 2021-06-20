2e9226260417861d42042a2cb8dc5a3d3c054f7c
Arun Jayaraman <riparun.aj@gmail.com>
2021-06-20 19:53:04 -0400

    Created a table view that can populate data from csv
    Also added a scrollbar for manuevering even though you can scroll without the scrollbar. However the scrollbar location needs to be fixed and the overall table still needs to be put inside the subframe

--- table.py
+++ table.py
@@ -33,7 +33,7 @@ root.rowconfigure(0, weight = 1)
 root.columnconfigure(0, weight = 1)
 
 
-tableFrame = Frame(root, background = 'red')
+tableFrame = Frame(root, background = mainColor)
 tableFrame.grid(row = 0, column = 0, sticky = "nsew")
 
 showFrame(tableFrame)
@@ -45,35 +45,49 @@ showFrame(tableFrame)
 activeUser = "test"
 
 # Create subframe
-viewFrame = Frame(tableFrame, bg = 'red')
+viewFrame = Frame(tableFrame, bg = accentColor)
 
 # Labels
 testTree = ttk.Treeview(tableFrame)
 
 # Define columns
-testTree['columns'] = ("Date", "Name", "Planned", "Actual")
+testTree['columns'] = ("Date", "Name", "Planned", "Actual", "Difference")
 
 # Format columns
-testTree.column("#0", width = 120, minwidth = 25)
+testTree.column("#0", width = 0, stretch = NO)
 testTree.column("Date", anchor = W)
 testTree.column("Name", anchor = W)
 testTree.column("Planned", anchor = E)
 testTree.column("Actual", anchor = E)
+testTree.column("Difference", anchor = E)
 
-testTree.heading("#0", text = "Label", anchor = W)
-testTree.heading("Date", text = "Date", anchor = W)
-testTree.heading("Name", text = "Name", anchor = W)
+
+#testTree.heading("#0", text = "Label", anchor = W)
+testTree.heading("Date", text = "Date", anchor = CENTER)
+testTree.heading("Name", text = "Name", anchor = CENTER)
 testTree.heading("Planned", text = "Planned", anchor = CENTER)
 testTree.heading("Actual", text = "Actual", anchor = CENTER)
-
+testTree.heading("Difference", text = "Difference", anchor = CENTER)
 #Add data
-testTree.insert(parent = '', index = 'end', iid = 0, text = "Parent", values = ("6/12", "Food", 300, 500))
+#testTree.insert(parent = '', index = 'end', iid = 0, text = "Parent", values = ("6/12", "Food", 300, 500))
 
+counter = 0
+with open('UserData/'+activeUser+'.csv', 'r') as file:
+        reader = csv.reader(file)
+        for line in reader:
+            print(line)
+            diff = float(line[3]) - float(line[2])
+            diff = str('-$' + str(-diff)) if diff < 0 else '$' + str(diff) 
+            tempTuple = (line[0], line[1], '$' + line[2], '$' + line[3], diff)
+            testTree.insert(parent = '', index = 'end', iid = counter, text = "Parent", values = tempTuple)
+            counter += 1
+
+for i in range(40):
+    testTree.insert(parent = '', index = 'end', iid = counter, text = "Parent", values = tempTuple)
+    counter += 1
 testTree.pack(pady = 20)
-
-
-
-
+vsb = ttk.Scrollbar(tableFrame, orient = 'vertical', command = testTree.yview)
+vsb.pack(side = 'right', fill = 'y')
 #endregion
 
 
