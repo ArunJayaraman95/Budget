021cf6679cb695b3f5a7c84827bb8bd0cfe6e06a
Arun Jayaraman <riparun.aj@gmail.com>
2021-06-20 02:11:08 -0400

    Converting registration account to grid
    Not done. Just in the process. It's not showing me any label placements so I will have to figure that out later

--- loginPage.py
+++ loginPage.py
@@ -45,7 +45,10 @@ root.title("Budget Boi")
 root.iconbitmap("img/WayneStateLogo.ico")
 
 #============Frame 1 ==============# 
-#Functions
+# Variables
+activeUser = ""
+
+# Functions
 def submitLogin():
     global activeUser
     print("Login submit button clicked")
@@ -103,33 +106,6 @@ submitButton = Button(loginMenu, text = "Submit", bg = "#A9e451", padx = 10, pad
 submitButton.grid(row = 5, column = 1, padx = 20, pady = 10, sticky = 'ew')
 registerButton = Button(loginMenu, text = "Make new account", font = ("Verdana", 10), bg = mainColor, command = lambda: showFrame(newAccountFrame))
 registerButton.grid(row = 5, column = 0, padx = 20, pady = 10, sticky = 'ew')
-"""
-#DELETED CANVAS
-
-# Logo image
-#logo = ImageTk.PhotoImage(Image.open("img/Dollar Sign.png").resize((120, 120)))
-#logoImage = Label(image = logo)
-#logoImage.place(x = sx/2, y = sy/2 - 270, anchor = N)
-
-
-activeUser = ""
-
-def popupLogin(message):
-    win = Toplevel()
-    win.wm_title("Window")
-    win.geometry("+%d+%d" % (sx/2 - 50, sy/2-200))
-    l = Label(win, text=message)
-    l.grid(row=0, column=0)
-
-    b = Button(win, text="Okay", command=win.destroy)
-    b.grid(row=1, column=0)
-
-
-
-
-
-
-"""
 
 
 
@@ -140,49 +116,10 @@ def popupLogin(message):
 
 
 
-
-
-
-
-
-"""
 # ===============Frame 2=====================#
-# Create canvas
-canvas = Canvas(newAccountFrame, width = sx, height = sy)
-canvas.pack()
-canvas.config(background=mainColor)
-
-# Form outline
-canvas.create_rectangle(sx/2 - 200, sy/6, sx/2 + 200, 2*sy/3-50, fill = accentColor, width = 0)
 
 
-# Create labels for login and place them
-loginLabel = Label(newAccountFrame, text = "Register!", font = usernameFont, bg = accentColor)
-loginLabel.place(x = sx/2, y = sy/2 - 350, anchor = N)
-usernameFont = font.Font(size = 16)
-usernameLabel = Label(newAccountFrame, text = "Username: ", font = usernameFont, bg = accentColor)
-usernameLabel.place(x = sx/2 - 155, y = sy/2 - 280, anchor = NW)
-loginLabel.config(font = ("Courier", 44))
-passwordLabel = Label(newAccountFrame, text = "Password: ", font = usernameFont, bg = accentColor)
-passwordLabel.place(x = sx/2 - 155, y = sy/2 - 210, anchor = NW)
-passwordConLabel = Label(newAccountFrame, text = "Confirm Password: ", font = usernameFont, bg = accentColor)
-passwordConLabel.place(x = sx/2 - 155, y = sy/2 - 140, anchor = NW)
-
-# Logo image
-#logo = ImageTk.PhotoImage(Image.open("img/Dollar Sign.png").resize((120, 120)))
-#logoImage = Label(image = logo)
-#logoImage.place(x = sx/2, y = sy/2 - 270, anchor = N)
-
-
-# Create input box for username and password
-urInput = Entry(newAccountFrame, width = 20, font = inputFont)
-urInput.place(x = sx/2, y = sy/2 - 250, anchor = N)
-prInput = Entry(newAccountFrame, width = 20, font = inputFont, show = '*')
-prInput.place(x=sx/2, y = sy/2 - 180, anchor = N)
-pcInput = Entry(newAccountFrame, width = 20, font = inputFont, show = '*')
-pcInput.place(x=sx/2, y = sy/2 - 110, anchor = N)
-
-
+# Functions
 def registerAccount():
     print("Register account clicked")
     # Store entries
@@ -198,22 +135,67 @@ def registerAccount():
             print(line)
             if line[0].lower().strip() == userEntry.lower():
                 print("User already exists")
-                popupLogin("USER Exists already")
                 foundFlag = True
     if not foundFlag:
         if passEntry != confEntry:
-            popupLogin("Passwords don't match")
+            messagebox.showwarning("Error", "Passwords don't match")
         else:
             with open ('UserData/userList.csv', 'a') as file:
                 writer = csv.writer(file, lineterminator="\n")
                 writer.writerow([userEntry, passEntry])
             print("User ", userEntry, "added!")
-            popupLogin("USER ADDED")
+            messagebox.showinfo("Success!", "User added!")
             
 
     urInput.delete(0, END)
     prInput.delete(0, END)
     pcInput.delete(0, END)
+
+widthAdjuster2 = 0.4
+heightAdjuster2 = 0.2
+registerMenu = Frame(newAccountFrame, width =0, height = sy/2, bg = accentColor)
+loginMenu.grid(row = 0, column = 0, padx = sx * widthAdjuster2, pady = sy * heightAdjuster2, ipadx = 0, ipady = 0)
+
+
+# Create labels for login and place them
+registerTitle = Label(registerMenu, text = "Register!", font = ("Courier", 60), bg = accentColor)
+registerTitle.grid(row = 0, column = 0, padx = 10, pady = 10, columnspan = 2, sticky = "ew")
+
+usernameLabel = Label(registerMenu, text = "Username: ", font = usernameFont, bg = accentColor)
+usernameLabel.grid(row = 1, column = 0, padx = 10, pady = 10, columnspan = 2,sticky = "w")
+
+passwordLabel = Label(registerMenu, text = "Password: ", font = usernameFont, bg = accentColor)
+passwordLabel.grid(row = 3, column = 0, padx = 10, pady = 10, columnspan = 2,sticky = 'w')
+
+passwordConLabel = Label(registerMenu, text = "Confirm Password: ", font = usernameFont, bg = accentColor)
+passwordConLabel.grid(row = 5, column = 0, padx = 10, pady = 10, columnspan = 2,sticky = 'w')
+
+
+# Create input box for username and password
+urInput = Entry(registerMenu, width = 20, font = inputFont)
+urInput.grid(row = 2, column = 0, padx = 10, pady = 10, columnspan = 2,sticky = 'ew')
+
+prInput = Entry(registerMenu, width = 20, font = inputFont, show = '*')
+prInput.grid(row = 4, column = 0, padx = 10, pady = 10, columnspan = 2,sticky = 'ew')
+
+pcInput = Entry(registerMenu, width = 20, font = inputFont, show = '*')
+pcInput.grid(row = 6, column = 0, padx = 10, pady = 10, columnspan = 2,sticky = 'ew')
+
+
+"""
+
+
+
+# Create input box for username and password
+urInput = Entry(newAccountFrame, width = 20, font = inputFont)
+urInput.place(x = sx/2, y = sy/2 - 250, anchor = N)
+prInput = Entry(newAccountFrame, width = 20, font = inputFont, show = '*')
+prInput.place(x=sx/2, y = sy/2 - 180, anchor = N)
+pcInput = Entry(newAccountFrame, width = 20, font = inputFont, show = '*')
+pcInput.place(x=sx/2, y = sy/2 - 110, anchor = N)
+
+
+
 
 #Create buttons
 confirmButton = Button(newAccountFrame, text = "Register", bg = "#A9e451", padx = 10, pady = 0, font = ("Verdana", 15), command = registerAccount)
