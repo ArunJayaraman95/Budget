72a8a6ac04a68c1d0620d4e9e51bd4c7639ae3e5
Arun Jayaraman <riparun.aj@gmail.com>
2021-06-20 19:21:39 -0400

    Inserting table view with junk data
    NOTE: table is fixed on tableframe, must do place method on viewframe and move table to viewframe

--- table.py
+++ table.py
@@ -2,7 +2,7 @@
 from tkinter import *
 from tkinter import messagebox
 import tkinter.font as font
-from PIL import ImageTk, Image
+from tkinter import ttk
 import csv
 
 # Define colors
@@ -20,20 +20,6 @@ def showFrame(frame):
 # Creating window
 root=Tk()
 root.state("zoomed")  #Makes it fullscreen automatically
-
-
-# Configurations
-root.rowconfigure(0, weight = 1)
-root.columnconfigure(0, weight = 1)
-
-loginFrame = Frame(root, background = mainColor)
-registerFrame = Frame(root, background = mainColor)
-
-for frame in (loginFrame, registerFrame):
-    frame.grid(row = 0, column = 0, sticky = "nsew")
-
-showFrame(registerFrame)
-
 sx = root.winfo_screenwidth() 
 sy = root.winfo_screenheight()
 
@@ -42,51 +28,51 @@ root.geometry("%dx%d" % (sx, sy))
 root.title("Budget Boi")
 root.iconbitmap("img/WayneStateLogo.ico")
 
+# Configurations
+root.rowconfigure(0, weight = 1)
+root.columnconfigure(0, weight = 1)
+
+
+tableFrame = Frame(root, background = 'red')
+tableFrame.grid(row = 0, column = 0, sticky = "nsew")
+
+showFrame(tableFrame)
+
+
 #region Frame1
 #============Frame 1 ==============# 
 # Variables
-activeUser = ""
-
-# Functions
-def submitLogin():
-    global activeUser
-    print("Login submit button clicked")
-    userEntry = uInput.get()
-    passEntry = pInput.get()
-    foundFlag = False
-    # Check username
-    print("Userentry", userEntry, uInput.get())
-    with open('UserData/userList.csv', 'r') as file:
-        reader = csv.reader(file)
-        for line in reader:
-            print(line)
-            if line[0].lower().strip() == userEntry.lower() and line[1].strip() == passEntry:
-                print("In file")
-                foundFlag = True
-                activeUser = userEntry
-                messagebox.showinfo("Success!", "Welcome " + activeUser + "!")
-                break
-        if not foundFlag:
-            print("Username (" + userEntry + ") not found")
-            messagebox.showwarning("User not found")
-            #warningLabel = Label(loginMenu, text = "Incorrect credentials", fg = "#FF890A", bg = accentColor)
-            #warningLabel.config(font = ("Verdana", 12))
-            #warningLabel.grid(row = 6, column = 1)
-    uInput.delete(0, END)
-    pInput.delete(0, END)
-    print("Active user:", activeUser)
+activeUser = "test"
 
 # Create subframe
-widthAdjuster = 0.4
-heightAdjuster = 0.2
-loginMenu = Frame(loginFrame, bg = accentColor)
-#loginMenu.grid(row = 0, column = 0, padx = sx * widthAdjuster, pady = sy * heightAdjuster, ipadx = 0, ipady = 0)
-#loginMenu.place(height = 500, width = 400, anchor = CENTER, rely = 0.5, relx = 0.5)
-loginMenu.pack()
-loginMenu.config()
+viewFrame = Frame(tableFrame, bg = 'red')
+
 # Labels
-loginTitle = Label(loginMenu, text = "Login", font = ("Courier", 80), bg = accentColor)
-loginTitle.grid(row = 0, column = 0, padx = 10, pady = 10, columnspan = 2, sticky = "ew")
+testTree = ttk.Treeview(tableFrame)
+
+# Define columns
+testTree['columns'] = ("Date", "Name", "Planned", "Actual")
+
+# Format columns
+testTree.column("#0", width = 120, minwidth = 25)
+testTree.column("Date", anchor = W)
+testTree.column("Name", anchor = W)
+testTree.column("Planned", anchor = E)
+testTree.column("Actual", anchor = E)
+
+testTree.heading("#0", text = "Label", anchor = W)
+testTree.heading("Date", text = "Date", anchor = W)
+testTree.heading("Name", text = "Name", anchor = W)
+testTree.heading("Planned", text = "Planned", anchor = CENTER)
+testTree.heading("Actual", text = "Actual", anchor = CENTER)
+
+#Add data
+testTree.insert(parent = '', index = 'end', iid = 0, text = "Parent", values = ("6/12", "Food", 300, 500))
+
+testTree.pack(pady = 20)
+
+
+
 
 #endregion
 
