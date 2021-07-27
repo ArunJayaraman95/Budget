25e2291901635f20be19b290a6dff5cb0148f518
KullsumaAkther <84944200+KullsumaAkther@users.noreply.github.com>
2021-07-26 23:24:04 -0400

    Add files via upload

--- alert.py
+++ alert.py
@@ -16,3 +16,8 @@ def alertSend_Email(to, subject, message):
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
+        
+        message = "expense is over and it needs to reduce expense."
+        alertSend_Email(
+        to='test@test.com',        # Receiver's email
+    
